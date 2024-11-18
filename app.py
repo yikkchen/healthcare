69a868ce0ad68fe680087fb58a2652f18e5670df
SAStommy <mingfungto@gmail.com>
2024-11-18 19:44:35 +0800

    大改

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # 設定健康建議的函數
-def get_health_advice(blood_pressure, blood_sugar, height, weight):
+def get_health_advice(blood_pressure_in, blood_pressure_out, blood_sugar, height, weight):
     # 將建議按健康指標分成字典形式
     fontLargestStrong = {'blood_pressure': [], 'blood_sugar': [], 'bmi': []}
     fontSecondStrong = {'blood_pressure': [], 'blood_sugar': [], 'bmi': []}
@@ -11,8 +11,9 @@ def get_health_advice(blood_pressure, blood_sugar, height, weight):
     fontNormal = {'blood_pressure': [], 'blood_sugar': [], 'bmi': []}
     
     # 血壓建議
-    if isinstance(blood_pressure, float):
-        if blood_pressure > 130:
+    if isinstance(blood_pressure_in, float) and isinstance(blood_pressure_out, float) :
+        blood_pressure = blood_pressure_in / blood_pressure_out
+        if blood_pressure > 120 / 80:
             fontLargestStrong['blood_pressure'].append("血壓太高>120/80")
             fontSecondStrong['blood_pressure'].append("！注意！")
             fontNormalStrong['blood_pressure'].append("正常情況下，你的血壓應該為：")
@@ -23,7 +24,7 @@ def get_health_advice(blood_pressure, blood_sugar, height, weight):
             fontNormalStrong['blood_pressure'].append("3.低鹽飲食：減少鈉鹽的攝取，可使血壓下降，飲食宜採清淡，盡量避免食用醃漬食物。")
             fontNormalStrong['blood_pressure'].append("4.控制飲酒：喝酒會使高血壓藥物失去療效。要避免血壓上升，飲酒量不宜超過30公克酒精。")
             fontNormalStrong['blood_pressure'].append("5.規律運動：每天30分鐘，一個星期最好五次以上，做一些安全溫和的有氧運動，可以改善血壓過高問題。")
-        elif blood_pressure < 90:
+        elif blood_pressure < 90 / 60:
             fontLargestStrong['blood_pressure'].append("血壓太低<90/60")
             fontSecondStrong['blood_pressure'].append("！注意！")
             fontNormalStrong['blood_pressure'].append("正常情況下，你的血壓應該為：")
@@ -44,28 +45,28 @@ def get_health_advice(blood_pressure, blood_sugar, height, weight):
     if isinstance(blood_sugar, float):
         if blood_sugar > 140:
             fontLargestStrong['blood_sugar'].append("血糖太高>140")
-            fontSecondStrong['blood_pressure'].append("！注意！")
-            fontNormalStrong['blood_pressure'].append("正常情況下，你的血糖應該為：")
-            fontNormalStrong['blood_pressure'].append("空腹血糖：70~100mg/dL")
-            fontNormalStrong['blood_pressure'].append("飯後兩小時血糖：70~140mg/dL")
-            fontSecondStrong['blood_pressure'].append("！改善！")
-            fontNormalStrong['blood_pressure'].append("1. 避免吃高升糖指數的食物，選擇 GI 值低於 55 的低升糖指數食物，例如：糙米、燕麥、山藥、大部分蔬菜、豆魚蛋肉類、小番茄、芭樂、木瓜等。")
-            fontNormalStrong['blood_pressure'].append("1. 避免吃含精緻糖的食物，像是冰糖、砂糖、高果糖漿等精製糖，避免食用麵包、冰淇淋、甜甜圈等含精緻糖的食物。")
-            fontNormalStrong['blood_pressure'].append("2. 避免吃高油脂食物，選擇植物油來替代動物性脂肪（如：豬油、牛油、奶油、培根等）。")
-            fontNormalStrong['blood_pressure'].append("4. 避免吃高鈉食物，並避免過度飲酒。")
+            fontSecondStrong['blood_sugar'].append("！注意！")
+            fontNormalStrong['blood_sugar'].append("正常情況下，你的血糖應該為：")
+            fontNormalStrong['blood_sugar'].append("空腹血糖：70~100mg/dL")
+            fontNormalStrong['blood_sugar'].append("飯後兩小時血糖：70~140mg/dL")
+            fontSecondStrong['blood_sugar'].append("！改善！")
+            fontNormalStrong['blood_sugar'].append("1. 避免吃高升糖指數的食物，選擇 GI 值低於 55 的低升糖指數食物，例如：糙米、燕麥、山藥、大部分蔬菜、豆魚蛋肉類、小番茄、芭樂、木瓜等。")
+            fontNormalStrong['blood_sugar'].append("1. 避免吃含精緻糖的食物，像是冰糖、砂糖、高果糖漿等精製糖，避免食用麵包、冰淇淋、甜甜圈等含精緻糖的食物。")
+            fontNormalStrong['blood_sugar'].append("2. 避免吃高油脂食物，選擇植物油來替代動物性脂肪（如：豬油、牛油、奶油、培根等）。")
+            fontNormalStrong['blood_sugar'].append("4. 避免吃高鈉食物，並避免過度飲酒。")
         elif blood_sugar < 70:
             fontLargestStrong['blood_sugar'].append("血糖太低<70")
-            fontSecondStrong['blood_pressure'].append("！注意！")
-            fontNormalStrong['blood_pressure'].append("正常情況下，你的血糖應該為：")
-            fontNormalStrong['blood_pressure'].append("空腹血糖：70~100mg/dL")
-            fontNormalStrong['blood_pressure'].append("飯後兩小時血糖：70~140mg/dL")
-            fontNormalStrong['blood_pressure'].append("請注意您身理上是否有虛弱、嗜睡、飢餓、臉色蒼白、冒冷汗、心跳加快、發冷、抽筋、頭暈等症狀；心理上可能會有情緒改變及行為改變。")
-            fontSecondStrong['blood_pressure'].append("！改善！")
-            fontNormalStrong['blood_pressure'].append("以下為可能造成低血糖的原因：")
-            fontNormalStrong['blood_pressure'].append("藥物：自行調整藥物劑量不正確、用藥時間不規律等。")
-            fontNormalStrong['blood_pressure'].append("飲食：三餐不定時、用藥後未進食等。")
-            fontNormalStrong['blood_pressure'].append("運動：空腹運動、過度運動等。")
-            fontNormalStrong['blood_pressure'].append("如果長時間處於低血糖狀態，建議就醫。")
+            fontSecondStrong['blood_sugar'].append("！注意！")
+            fontNormalStrong['blood_sugar'].append("正常情況下，你的血糖應該為：")
+            fontNormalStrong['blood_sugar'].append("空腹血糖：70~100mg/dL")
+            fontNormalStrong['blood_sugar'].append("飯後兩小時血糖：70~140mg/dL")
+            fontNormalStrong['blood_sugar'].append("請注意您身理上是否有虛弱、嗜睡、飢餓、臉色蒼白、冒冷汗、心跳加快、發冷、抽筋、頭暈等症狀；心理上可能會有情緒改變及行為改變。")
+            fontSecondStrong['blood_sugar'].append("！改善！")
+            fontNormalStrong['blood_sugar'].append("以下為可能造成低血糖的原因：")
+            fontNormalStrong['blood_sugar'].append("藥物：自行調整藥物劑量不正確、用藥時間不規律等。")
+            fontNormalStrong['blood_sugar'].append("飲食：三餐不定時、用藥後未進食等。")
+            fontNormalStrong['blood_sugar'].append("運動：空腹運動、過度運動等。")
+            fontNormalStrong['blood_sugar'].append("如果長時間處於低血糖狀態，建議就醫。")
         else:
             fontLargestStrong['blood_sugar'].append("Your blood sugar is normal. Keep it up!")
     else:
@@ -96,13 +97,14 @@ def index():
 @app.route('/result', methods=['POST'])
 def result():
     # 取得表單數據
-    blood_pressure = float(request.form.get('blood_pressure')) if request.form.get('blood_pressure') else None
+    blood_pressure_in = float(request.form.get('blood_pressure_in')) if request.form.get('blood_pressure_in') else None
+    blood_pressure_out = float(request.form.get('blood_pressure_out')) if request.form.get('blood_pressure_out') else None
     blood_sugar = float(request.form.get('blood_sugar')) if request.form.get('blood_sugar') else None
     height = float(request.form.get('height')) if request.form.get('height') else None
     weight = float(request.form.get('weight')) if request.form.get('weight') else None
 
     # 取得健康建議
-    fontLargestStrong, fontSecondStrong, fontNormalStrong, fontNormal = get_health_advice(blood_pressure, blood_sugar, height, weight)
+    fontLargestStrong, fontSecondStrong, fontNormalStrong, fontNormal = get_health_advice(blood_pressure_in, blood_pressure_out, blood_sugar, height, weight)
 
     return render_template(
         'result.html',
